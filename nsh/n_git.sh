c77660675123ca6ea93c2112b1afb056a033ab20
mwmcgilvery <mwmcgilvery@gmail.com>
2019-12-21 13:25:38 -0800

    Updates to the mobile nm repository

--- nsh/n_git.sh
+++ nsh/n_git.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-cd ~/nmbl;
-hub pull;
-hub add --all;
-hub commit -m "Updates to the mobile nmbl repository";
-hub push;
-hub status > ~/nmbl/nsudo/ngits.txt;
-hub log > ~/nmbl/nsudo/ngitl.txt;
+cd /storage/emulated/0/ngt;
+echo syncing github repos
+echo adding files to local index
+hub add *;
+echo commiting changes to HEADfile
+hub commit -m "Updates to the mobile nm repository";
+echo pushing files to remote repo
+hub push origin master;
+hub status > /storage/emulated/0/ngt/nsudo/ngits.txt;
+cd nsudo && micro ngits.txt;
+hub log > /storage/emulated/0/ngt/nsudo/ngitl.txt;
 echo nmbl git syncronized;
