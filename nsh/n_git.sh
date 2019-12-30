deaf8d5c8bc59ba8894e279276f3e4432940f4f6
mwmcgilvery <mwmcgilvery@github.com>
2019-12-30 11:06:52 -0800

    Mobile updates

--- nsh/n_git.sh
+++ nsh/n_git.sh
@@ -3,7 +3,7 @@ cd /storage/emulated/0/ngt;
 echo syncing github repos
 echo adding files to local index
 hub add .; 
-hub pull .; 
+hub pull origin master; 
 echo commiting changes to HEADfile;
 hub commit -m "Mobile updates"; 
 echo pushing files to remote repo;
