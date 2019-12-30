cf2eaa0e4299d9e95b3e47450b8e3f34ab7658eb
mwmcgilvery <mwmcgilvery@github.com>
2019-12-30 11:04:33 -0800

    Mobile updates

--- nsh/n_git.sh
+++ nsh/n_git.sh
@@ -2,12 +2,10 @@
 cd /storage/emulated/0/ngt;
 echo syncing github repos
 echo adding files to local index
-hub add *;
-echo commiting changes to HEADfile
-hub commit -m "Updates to the mobile nm repository";
-echo pushing files to remote repo
-hub status > /storage/emulated/0/ngt/nsudo/ngits.txt;
+hub add .; 
+hub pull .; 
+echo commiting changes to HEADfile;
+hub commit -m "Mobile updates"; 
+echo pushing files to remote repo;
 hub push origin master;
-cd nsudo && micro ngits.txt;
-hub log > /storage/emulated/0/ngt/nsudo/ngitl.txt;
 echo nmbl git syncronized;
