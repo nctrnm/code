95337e1984fee87570c9a7debd14c4e0c6bfca4c
mwmcgilvery <mwmcgilvery@github.com>
2020-01-10 17:22:29 -0800

    Mobile updates

--- nsh/nsawtri50.sh
+++ nsh/nsawtri50.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sox -n  nsawtri50.mp3 synth 3 saw 50 synth 5 tri 50 && sox nsawtri50.mp3 -c1  nsawtri50.mp3 sinc -441.441 sinc 444.1 && play nsawtri50.mp3
-ffmpeg -i -ac 1 -ab 44100 nsawtri50.mp3 nsawtri50.wav
+echo 'enter freq 1'
+read f1
+echo 'enter freq 2'
+read f2
+sox -n  nreesex.wav synth 3 saw $f1 synth 5 sin $f2 && sox nreesex.wav -c1  nreesex.wav lowpass 441.441 && play nreesex.wav
