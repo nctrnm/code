a08e69f5e1d1a4216f664f51f69c9aafa5faf1d6
mwmcgilvery <mwmcgilvery@github.com>
2020-03-03 15:14:27 -0800

    u

--- nsh/nbash.sh
+++ nsh/nbash.sh
@@ -1,11 +1,8 @@
 #Author: Matthew McGilvery
-#License: LGPL 3.0(or later)
+#License: GPL 3 or Later
 #!/bin/bash/
 bash /sdcard/ngt/nsh/npress.sh
-cd /sdcard/ngt && bash /sdcard/ngt/nsh/ngt.sh
-wait 
-bash /sdcard/ngt/nsh/npress.sh
-date "+%D %T"|fmt -u -w 22 > /sdcard/.ntimelog && date "+%D %T"|fmt -u -w 22 > /sdcard/ngt/ntime.log
+date "+%D %T"|fmt -u -w 22 > /sdcard/.ntimelog && date "+%D %T"|fmt -u -w 22 > /sdcard/ngt/TIME.log
 alias github='termux-open https://github.com/mwmcgilvery'
 alias sxh='sox --help-effect'
 alias x='exit'
@@ -19,6 +16,7 @@ alias soxh='sox --help-effect'
 alias h='cd /sdcard/ && pwd'
 alias usr='echo $(whoami)'
 alias o='termux-open'
+alias ls='exa' && alias list='exa -l'
 alias snd='termux-open --send'
 alias srch='pkg search'
 alias shcp='cp ~/.bashrc /sdcard/nbash.sh'
@@ -30,17 +28,26 @@ alias hm='cd /sdcard/ && pwd'
 alias bashed='vim ~/.bashrc ; cp ~/.bashrc /sdcard/nbash.sh'
 alias ddir='rm -r '
 alias ngit='bash /sdcard/ngt/nsh/ngt.sh'
+alias rec='termux-microphone-record -l 0'
 alias ngt='cd /sdcard/ngt && pwd'
-alias gthb='w3m https://github.com/mwmcgilvery'
-alias google='w3m https://www.google.com/search?q=' 
+alias gthb='lynx https://github.com/mwmcgilvery'
+alias google='lynx https://www.google.com/search?q=' 
 alias pkgi='pkg install'
 alias dcc='ccrypt -d'
 alias cc='ccrypt -e'
 alias end='kill -9'
 alias nt='vim'
-alias google='w3m https://google.com/search?q='
+alias google='lynx https://google.com/search?q='
 alias fmh='nnn /sdcard/ngt'
 alias gitp='git push'
 alias _tab='tmux new -c ntm'
 alias go='cd '
 alias fm='nnn'
+echo "sync git repo?"
+read ans
+if [[ "$ans" = "y" ]]||[[ "$ans" = "Y" ]]
+then
+	cd /sdcard/ngt && bash /sdcard/ngt/nsh/ngt.sh; $h && pwd
+else
+	$h && pwd
+fi
