fde9fe1b09ab130789f8fe565bc10e59b974a0e4
mwmcgilvery <mwmcgilvery@github.com>
2020-03-10 10:32:28 -0700

    u i improved the efficiency of my git sync file

--- n.sh/n.gt.sh
+++ n.sh/n.gt.sh
@@ -1,14 +1,10 @@
 #Author:Matthew McGilvery
-#License: LGPL3 OR LATER
+#License: GPL3 OR LATER
 #!/bin/bash
-$h
-#pull any remote updates
-hub pull
-#add local files
-hub add --all; hub push
-#commiting and pushing local changes to HEADfile;
-hub commit -a; hub push
-#log changes to logfile
-hub log|uniq > /sdcard/n.doc/ngitl.txt
-#print February git logs sorted in reverse chronological order (most recent at bottom)
-cat /sdcard/n.doc/ngitl.txt|grep -e "$(echo "$(date +%b) $(date +%d)")"|uniq|sort
+#nctrnm.com
+#Git Sync
+#pull remote updates
+cd /sdcard/n.git/ && pwd && hub pull
+#add&commit&push local changes to remote;
+hub add --all && hub commit -a &&  hub push
+$date +%b +%d|uniq|sort
