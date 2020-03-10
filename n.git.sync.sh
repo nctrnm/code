f1c232914192f0a89161bdbb0389710ed9f4f4ad
mwmcgilvery <mwmcgilvery@github.com>
2020-03-10 12:51:23 -0700

    u

--- n.git.sync.sh
+++ n.git.sync.sh
@@ -1,6 +1,7 @@
-#Matthew McGilvery                       
-#GPL 3 or Later                        
-#nctrnm.com
+#Author: Matthew McGilvery
+#License: GPL 3 or Later
+#link: nctrnm.com
+#!/bin/bash/
 #Git Sync
-#!/bin/bash
-git pull && git add --all && git commit && git push 
+hub pull && hub add --all && hub commit ;
+hub push && hub status && github&
