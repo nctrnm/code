e7b5c7f4331f1f2af5b560ba169d48b3882e9704
mwmcgilvery <mwmcgilvery@github.com>
2020-03-10 10:45:40 -0700

    u

--- n.sh/n.git.sync.sh
+++ n.sh/n.git.sync.sh
@@ -2,9 +2,10 @@
 #License: GPL3 OR LATER
 #!/bin/bash
 #nctrnm.com
-#Git Sync
-#pull remote updates
+#Git Sync Script
+
+#Pull remote updates
 cd /sdcard/n.git/ && pwd && hub pull
-#add&commit&push local changes to remote;
+#Add&commit&push local changes to remote;
 hub add --all && hub commit -a &&  hub push
-$date +%b +%d|uniq|sort
+cd /sdcard/
