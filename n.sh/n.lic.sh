8619000dcc3a57d7dc197c96b3071b4760a9245f
Matthew McGilvery <48932877+mwmcgilvery@users.noreply.github.com>
2020-03-13 13:38:42 -0700

    d

--- n.sh/n.lic.sh
+++
@@ -1,8 +0,0 @@
-#Author: Matthew McGilvery, License: LGPL3
-#!/bin/bash
-echo 'enter file type' 
-read type
-for file in *.${type}; do
-  sed -i '1i #Author:Matthew McGilvery' "$file" &&
-  echo '#License: LGPL3' >> "$file"
-done
