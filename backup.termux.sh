ca1ec05f0fb8d8dd0d20cf268b663c7eaf267fb4
mwmcgilvery <mwmcgilvery@github.com>
2020-03-13 15:17:48 -0700

    u

--- backup.termux.sh
+++ backup.termux.sh
@@ -1,7 +1,8 @@
-#Author: Matthew McGilvery
+#Matthew McGilvery
 #License: GPL 3 or Later
 #link: nctrnm.com
 #!/bin/bash/
+#3.13.20
 #Termux Backup Script
 termux-setup-storage
 cd /data/data/com.termux/files
--- n.git.sync.sh
+++ n.git.sync.sh
@@ -1,6 +1,7 @@
-#Author: Matthew McGilvery
+#Matthew McGilvery
 #License: GPL 3 or Later
 #link: nctrnm.com
+#3.9.20
 #!/bin/bash/
 #Git Sync
 hub pull && hub add --all && hub commit ;
