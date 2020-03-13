b2c3aa204ce9509d0af0b00bd2150d228efcc790
Matthew McGilvery <48932877+mwmcgilvery@users.noreply.github.com>
2020-03-13 13:23:18 -0700

    u

---
+++ n.sh/n.keyb.sh
@@ -0,0 +1,7 @@
+#Matthew McGilvery
+#3.8.2020
+#GPL3 
+#nctrnm.com 
+#Keybindings
+#!/bin/bash
+bind -P|sort|uniq
--- n.sh/n.keybind.sh
+++
@@ -1,5 +0,0 @@
-#Author:Matthew McGilvery
-#!/bin/bash
-#License: LGPL3 orlater
-#Print Keybindings
-bind -P|sort|uniq
