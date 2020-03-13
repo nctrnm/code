a325a983f4b18ea3dfd06427a7238e6a72fd404b
Matthew McGilvery <48932877+mwmcgilvery@users.noreply.github.com>
2020-03-13 12:47:37 -0700

    u

---
+++ n.sh/array.fmt.sh
@@ -0,0 +1,10 @@
+#Matthew McGilvery
+#GPL3 
+#nctrnm.com 
+#Bash Array Format
+#u: 3.13.20
+#!/bin/bash/
+numbers=(1 2 3 numbers hello world);
+for f in numbers;
+do echo ${numbers[$[f]]};
+done;
--- n.sh/n.array.sh
+++
@@ -1,8 +0,0 @@
-#Author:Matthew McGilvery
-#! /bin/bash/;
-numbers=(1 2 3 numbers hello world);
-echo ${numbers[1]};
-echo ${numbers[2]};
-echo ${numbers[3]};
-echo ${numbers[4]};
-echo ${numbers[5]};#License: LGPL3
