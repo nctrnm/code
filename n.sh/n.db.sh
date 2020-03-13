2892490385ca31339624715005b900d37426c5c4
Matthew McGilvery <48932877+mwmcgilvery@users.noreply.github.com>
2020-03-13 13:28:55 -0700

    Delete n.db.sh

--- n.sh/n.db.sh
+++
@@ -1,2 +0,0 @@
-#Author:Matthew McGilvery
-mv 'DanceBass A-.wav' 'DanceBass A.wav' 'DanceBass C+.wav' 'DanceBass C.wav' db                           'DanceBass F#+.wav' 'DanceBass F#.wav'#License: LGPL3
