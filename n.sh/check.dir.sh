102ee3a9e8b039f384a0cc189dec6e60fa912970
Matthew McGilvery <48932877+mwmcgilvery@users.noreply.github.com>
2020-03-13 13:17:08 -0700

    u

--- n.sh/check.dir.sh
+++ n.sh/check.dir.sh
@@ -1,11 +1,16 @@
-#Author:Matthew McGilvery
+#Matthew McGilvery
+#3.13.2020
+#GPL3 
+#nctrnm.com 
+#Directory Check w/ Error Forwards
 #!/bin/bash
+cd /storage/emulated/0/
 echo "Directory Check" 
 # test if /sdcard/ exists
 
-if test -e /x/y/x; then
+if test -e /sdcard/; then
   echo "Alright man..." >&2
 else
   echo "Yuck! Where is it??" >&2
   exit 1
-fi#License: LGPL3
+fi
