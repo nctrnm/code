97254e31db5daf48b27dd74a22e4b84af733a68c
mwmcgilvery <mwmcgilvery@github.com>
2020-03-15 16:34:54 -0700

    u

--- n.sh/n.synth.sh
+++ n.sh/n.synth.sh
@@ -1,9 +1,11 @@
-#Author: Matthew McGilvery
-#License: LGPL 3.0(or later)
+#Matthew McGilvery
+#GPL 3.0
+#nctrnm.com
+#Sin/Saw Synth Script
 #!/bin/bash
 echo enter frequency 1
 read f1
 echo enter frequency 2
 read f2
-sox -S -V -n /sdcard/ntmp/nsynth$f1$f2.mp3 synth 3 sin $f1 saw $f2 lowpass $(("$f2" * 3 )) highpass $(("$f1" * 2)) overdrive .01 norm -3 dither
-sox /sdcard/ntmp/nsynth$f1$f2.mp3 /sdcard/nsynth${RANDOM}.wav norm -1 dither
+sox -S -V -n -r44100 -b16 -c1 /sdcard/nsynth$f1$f2.mp3 synth 3 sin $f1 saw $f2 lowpass 220 .88q highpass 44.1 remix - overdrive 3 norm -3 fade 00:0.33 00:2.77  dither;
+play /sdcard/nsynth$f1$f2.mp3 norm -1 dither
