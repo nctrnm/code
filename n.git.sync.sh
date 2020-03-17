3a3d193f28668f0cc738a5e3585cc7420140aaea
mwmcgilvery <48932877+mwmcgilvery@users.noreply.github.com>
2020-03-16 17:35:55 -0700

    u

--- n.git.sync.sh
+++ n.git.sync.sh
@@ -3,6 +3,6 @@
 #link: nctrnm.com
 #3.9.20
 #!/bin/bash/
-#Git Sync
-hub pull && hub add --all && hub commit ;
-hub push && hub status
+#Git Sync	
+git pull && git add --all && git commit ;
+git push && git status
