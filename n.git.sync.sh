55b46742b0a17848266bdbb7f0aa72c1c7985018
mwmcgilvery <48932877+mwmcgilvery@users.noreply.github.com>
2020-04-30 23:24:20 -0700

    u

--- n.git.sync.sh
+++ n.git.sync.sh
@@ -2,7 +2,8 @@
 #License: GPL 3 or Later
 #link: nctrnm.com
 #3.9.20
-#!/bin/bash/
+#!/bin/bash
 #Git Sync	
-git pull && git add --all && git commit ;
-git push && git status
+git pull && git add --all && git commit;
+git push && git status;
+pwd;
