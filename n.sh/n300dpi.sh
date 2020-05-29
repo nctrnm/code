a246745fae32122b273cb0f0b8d59b0bb22fd038
* <48932877+mwmcgilvery@users.noreply.github.com>
2020-05-29 14:57:05 -0700

    a

---
+++ n.sh/n300dpi.sh
@@ -0,0 +1,9 @@
+#Matthew McGilvery
+#File Resize (300 dpi && 1500 pixel square crop
+#nctrnm.com
+#GPL3
+#!/bin/bash
+echo 'enter file' && read a 
+echo 'enter saved file name' && read b 
+convert -units PixelsPerInch a -resample 300 -resize 1500x1500 b
+$o b
