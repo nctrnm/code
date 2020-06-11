b5adaeea9cadabd6e6987e63a95ccbdaae50c37a
nctrnm <48932877+nctrnm@users.noreply.github.co>
2020-06-10 17:25:25 -0700

    u

--- n.bashrc.win10.sh
+++ n.bashrc.win10.sh
@@ -1,27 +1,30 @@
+
+
 #Author: Matthew McGilvery
 #License: GPL 3 or Later
 #link: nctrnm.com
 #!/bin/bash/
 #Bash Initialization File 
-stty rows 1280  && stty columns 720 
-export PS1='j:\j,u:\u,$: '
+export PS1='cd: $PWD, $: '
 sleep 5
-date "+%D %T"|fmt -u -w 22 > ~/.ntimelog && date "+%D %T"|fmt -u -w 22 > ~/nmoto/TIME.log
+date "+%D %T"|fmt -u -w 22 > /mnt/c/Users/mwmcgilvery/nmoto/.ntimelog && date "+%D %T"|fmt -u -w 22 > /mnt/c/Users/mwmcgilvery/nmoto/TIME.log
+alias shed='sudo vim ~/.bashrc'
 alias s.h='sox --help-effect'
+alias start='sudo -i bash'
 alias n.sig='pgrep -u u0_a224' && alias x='kill -9 $(nsig|fmt -u)'
 alias n.s='sox -SV -r44.1k -b16 -c1'
 alias p.y='python3'
 alias n.p='play'
+alias n.h='cd /mnt/c/Users/mwmcgilvery/'
 alias usr='echo $USER'
-alias n.h='~/nmoto && pwd'
 alias list='ls -l'
-alias n.ls='bash ~/nmoto/n.ls.sh'
+alias n.ls='bash /mnt/c/Users/mwmcgilvery/nmoto/n.ls.sh'
 alias srch='pkg search'
-alias shcp='cp ~/.bashrc ~/nmoto/n.sh/n.bash.rc.win10.sh'
+alias shcp='cp ~/.bashrc /mnt/c/Users/mwmcgilvery/nmoto/n.sh/n.bash.rc.win10.sh'
 alias s.git='git status' && alias p.git='git push'
 alias list='ls -bhtx' && alias apps='ls -b /bin'
 alias n.b='source ~/.bashrc'
-alias n.sh='vim ~/.bashrc ; cp ~/.bashrc ~/nmoto/n.bash.rc.win10.sh'
+alias n.sh='vim ~/.bashrc ; cp ~/.bashrc /mnt/c/Users/mwmcgilvery/nmoto/n.bash.rc.win10.sh'
 alias d.dir='rm -r'
 alias n.rec='sox -r 44100 -b 16 ~/${RANDOM}.wav'
 alias n.gt='cd ~/nmoto && pwd && ls -h' && alias n.git='bash ~/nmoto/n.git.sync.sh'
@@ -33,8 +36,9 @@ alias d.c='ccrypt -d'
 alias c.c='ccrypt -e'
 alias end='kill -9'
 alias n.t='vim'
+alias home='cd /mnt/c/Users/mwmcgilvery'
 alias ggl="lynx https://google.com/search?q=-"
 alias p.git='git push'
 alias n.tab='tmux new -c ntm'
 alias f.m='nnn'
-cd ~/nmoto && bash ~/nmoto/n.git.sync.sh && cd ~ &&  pwd && ls -bh . && $shcp;
+cd /mnt/c/Users/mwmcgilvery/nmoto && bash /mnt/c/Users/mwmcgilvery/nmoto/n.git.sync.sh && $shcp;
