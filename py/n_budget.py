25fc21f50dbab8d12f0fbc01281dd0c1a3ac4dd2
nctrnm <48932877+nctrnm@users.noreply.github.com>
2020-08-02 11:07:42 -0700

    added a budgeting script for my fellow gig workers, and freelancers

---
+++ py/n_budget.py
@@ -0,0 +1,18 @@
+#Matthew McGilvery
+#nctrnm.com
+#GPL3
+#Personal Economics Marginal Budget Script
+#this is a script that I need to budget my revenue as a gig worker
+#!/bin/python
+#budget: b, needs: n, wants: w, savings: s
+#you enter your budget here or the total revenue for the given time period OR revenue event
+b = input('Enter your budget: ')
+#n= needs expenditure
+n=int(b)/2
+#w= wants expenditure
+w=int(b)/5
+#s = savings expenditure
+s=int(b)/3
+#print out of aforementioned budget variables
+print('budget: ' + str(b) + ', needs: ' + str(n) + ', wants: ' + str(w) + ', savings: ' + str(s))
+
