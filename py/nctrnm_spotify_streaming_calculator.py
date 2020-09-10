b1fcf0590972478c8e23c3503f6cdacb8e96a328
Matthew McGilvery <48932877+nctrnm@users.noreply.github.com>
2020-09-10 09:00:30 -0700

    u a added a script
    a script that converts Spotify streams into USD .

--- py/nctrnm_spotify_streaming_calculator.py
+++ py/nctrnm_spotify_streaming_calculator.py
@@ -1,3 +1,7 @@
+#Matthew McGilvery
+#Spotify Stream to USD Calculator
+#GPL3orLater
+#nctrnm.com
 a= input('Enter number of streams: ')
 money =  a * 0.0075
-print('For ' + str(a) + ' streams, youd earn, ' + str(money) + ' in USD.')
+print('For ' + str(a) + ' streams, youd earn, $' + str(money) + ' in USD.')
