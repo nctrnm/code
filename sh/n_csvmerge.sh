05facc230603f08d3f2e8b5e032e6f2a38b84b72
nctrnm <48932877+nctrnm@users.noreply.github.com>
2020-11-29 12:12:51 -0800

    u logging into linux desktop terminal

---
+++ sh/n_csvmerge.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#Matthew McGilvery
+#11.29.2020
+#CSV Merge
+#Sourced from https://stackoverflow.com/questions/24641948/merging-csv-files-appending-instead-of-merging/24643455
+#
+output='nctrnm_sm_nov2020.csv'                       # Edit output name
+i=0                                       # Reset a counter
+for filen in ./*.csv; do #edit this  
+ if [ "$filen"  != "$output" ] ;      # Avoid recursion 
+ then 
+   if [[ $i -eq 0 ]] ; then 
+      head -1  "$filename" >   "$OutFileName" # Copy header if it is the first file
+   fi
+   tail -n +2  "$filename" >>  "$OutFileName" # Append from the 2nd line each file
+   i=$(( $i + 1 ))                            # Increase the counter
+ fi
+done
+
