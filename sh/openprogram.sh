0b10305ee2e2790c5bb7febfbddcda1ca0a7cd45
nctrnm <48932877+nctrnm@users.noreply.github.com>
2021-01-09 12:31:13 -0800

    u logging into linux desktop terminal

---
+++ sh/openprogram.sh
@@ -0,0 +1,10 @@
+#Matthew McGilvery
+#Program Opener for Linux Command Line
+#1.9.2021
+#nctrnm.com
+#!/bin/bash
+echo "enter desired program"
+read programx
+$programx --no-sandbox &> /dev/null &
+
+
