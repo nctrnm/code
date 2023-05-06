2cb9b779a8496cd0b9300a908e67b1ced1e8f118
Matthew McGilvery <48932877+nctrnm@users.noreply.github.com>
2023-05-06 15:56:35 -0700

    a
    Signed-off-by: Matthew McGilvery <48932877+nctrnm@users.noreply.github.com>

--- sh/txt2vox.sh
+++ sh/txt2vox.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
-# Script Name: text-to-conversational-male-voice.sh
+# Script Name: txt2vox.sh
 # Author: Matthew McGilvery
-# Date: 4/3/2023
+# Date: 5/6/2023
 # Description: This script converts text or a text file to a conversational male voice file using the macOS 'say' command and 'sox' tool.
-
+#!/bin/bash
 # Define the default input text
 default_text="Hello, this is a test of the conversational male voice."
 
@@ -21,12 +19,17 @@ if [[ -f "$input" ]]; then
   # Use the file as input
   input_file="$input"
   default_output="${input_file%.*}.wav"
-  say -v "Alex" -o temp.aiff -f "$input_file"
+  read -p "Enter the voice to use (e.g. Alex, Fred, Samantha): " voice
+  say -v "$voice" -o temp.aiff -f "$input_file"
 else
   # Use the text as input
   default_output="output.wav"
-  say -v "Alex" -o temp.aiff "$input"
+  read -p "Enter the voice to use (e.g. Alex, Fred, Samantha): " voice
+  say -v "$voice" -o temp.aiff "$input"
 fi
+
+# Play a section of the 'say' output file
+afplay -t 5 temp.aiff
 
 # Prompt the user for the output file name
 read -p "Enter the output file name (leave blank for default \"$default_output\"): " output
@@ -36,8 +39,13 @@ if [[ -z "$output" ]]; then
   output="$default_output"
 fi
 
+# Prompt the user for the speed, pitch, and normalization values
+read -p "Enter the speed value (default is 1.0): " speed
+read -p "Enter the pitch value (default is 0): " pitch
+read -p "Enter the normalization value (default is -1): " norm
+
 # Use 'sox' to convert the AIFF file to a WAV file
-sox temp.aiff "$output"
+sox temp.aiff "$output" speed "${speed:-1.0}" pitch "${pitch:-0}" norm "${norm:--1}" dither -a  
 
 # Clean up the temporary AIFF file
 rm temp.aiff
